4c19a1e743611f0d31546cc77996cd2c92d14216
Deadsg <144394753+Deadsg@users.noreply.github.com>
2025-10-19 12:09:28 -0400

    updated nix...

--- DEADSGOLD/flake.nix
+++ DEADSGOLD/flake.nix
@@ -5,10 +5,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
+
+    mySolders = pkgs.python3Packages.buildPythonPackage rec {
+      pname = "solders";
+      version = "0.19.0"; # Update to actual latest if needed
+
+      src = pkgs.fetchPypi {
+        inherit pname version;
+        hash = "sha256-8403f59b0b49b8db1875ea383c58f249"; # Replace with real hash from PyPI
+      };
+
+      doCheck = false;
+      # pythonImportsCheck = [ "solders" ];
+    };
   in {
     devShells.${system}.default = pkgs.mkShell {
       name = "deadsgold";
@@ -18,8 +30,12 @@
           flask
           requests
           numpy
+          tkinter
+          cryptography
+          base58
         ]))
         pkgs.git
+        mySolders
       ];
 
       shellHook = ''
